fd4b0645dd867c6f52410ac7fa90b0448a2b18bb
pilingyan <463668874@qq.com>
2016-12-14 14:32:26 +0800

    Init

---
+++ Test/Assets/Scripts/FPSMonitor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// 帧率监测器
+/// </summary>
+public class FPSMonitor : MonoBehaviour {
+    public GUISkin Skin;
+    public float FPS;
+    private float _currTime;
+    private float _nextTime;
+    private float _frameCount;
+
+    private void Start() {
+        _nextTime = Time.realtimeSinceStartup + 1;
+    }
+
+    private void OnGUI() {
+#if !RELEASE
+             if( Time.realtimeSinceStartup > _nextTime ) {
+            FPS = Time.frameCount - _frameCount;
+            _frameCount = Time.frameCount;
+            _nextTime = Time.realtimeSinceStartup + 1;
+        }
+
+             GUILayout.Label(string.Format("<color=red>FPS : {0}</color> ", FPS), Skin.label);
+#endif
+   
+    }
+}
